610fdb9d9373bbdab15cac1515285afa3382c13a
devRaxx <rpcastaneros@up.edu.ph>
2025-06-16 17:51:54 +0800

    feat: added backend config, controllers, models, and routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
-});
+app.use(express.json()); //allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
+  connectDB();
   console.log("Server started at http://localhost:5000");
 });
