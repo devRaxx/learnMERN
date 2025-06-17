3b0a0a2f1eea9606132cd33cb8449c79146da117
devRaxx <rpcastaneros@up.edu.ph>
2025-06-17 16:09:44 +0800

    feat(frontend): added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,18 @@
-import { Box, Button } from "@chakra-ui/react";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CreatePage from "./pages/CreatePage";
-import Navbar from "./components/Navbar";
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
 
 function App() {
   return (
-    <>
-      <Box minH={"100vh"}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreatePage />} />
-        </Routes>
-      </Box>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { CiSquarePlus } from "react-icons/ci";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    console.log("Dark mode toggled:", !isDarkMode);
+  };
+
+  return (
+    <nav className="flex justify-between items-center w-full fixed top-0 left-0 px-20 py-4">
+      <a href="/" className="font-bold text-2xl">
+        PRODUCT STORE 🛒
+      </a>
+      <div className="flex gap-4">
+        <a href="/create">
+          <CiSquarePlus size={50} />
+        </a>
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? (
+            <MdDarkMode size={50} />
+          ) : (
+            <MdOutlineLightMode size={50} />
+          )}
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
